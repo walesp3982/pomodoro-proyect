ccc0e385800c0a49adb3d59329ed149f1b6531e1
Walter Espejo <josuewalterespejoguarachi@gmail.com>
2026-04-26 15:52:56 -0400

    new timer with more funcionalitiy

--- src/hooks/timer.hook.ts
+++ src/hooks/timer.hook.ts
@@ -2,34 +2,36 @@ import { useEffect, useState } from "react";
 
 interface UseTimerReturn {
     seconds: number
+    isRunning: boolean
+
     action: {
         start: () => void,
         reset: () => void,
         stop: () => void,
-        isRunning: boolean
     }
 }
 export function useTimer(start_seconds: number = 0): UseTimerReturn {
-  const [seconds, setSeconds] = useState(start_seconds);
-  const [running, setRunning] = useState<boolean>(true);
+    const [seconds, setSeconds] = useState(start_seconds);
+    const [running, setRunning] = useState<boolean>(true);
 
 
-  useEffect(() => {
-    if(!running) return
-    const interval = setInterval(() => {
-      setSeconds((prev) => prev + 1)
-    }, 1000)
+    useEffect(() => {
+        if (!running) return
+        const interval = setInterval(() => {
+            setSeconds((prev) => prev + 1)
+        }, 1000)
 
-    return () => clearInterval(interval)
-  }, [running])
+        return () => clearInterval(interval)
+    }, [running])
 
-  return {
-    seconds: seconds,
-    action: {
-        start: () => setRunning(true),
-        stop: () => setRunning(false),
-        reset: () => setSeconds(0),
+    return {
+        seconds: seconds,
         isRunning: running,
+
+        action: {
+            start: () => setRunning(true),
+            stop: () => setRunning(false),
+            reset: () => setSeconds(0),
+        }
     }
-  }
 }
