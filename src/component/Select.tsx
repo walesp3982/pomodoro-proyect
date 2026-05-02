3801a78d4adc9bd7181d7ef0602afcc1a87893d0
Walter Espejo <josuewalterespejoguarachi@gmail.com>
2026-05-01 22:21:36 -0400

    rounded select theme

--- src/component/Select.tsx
+++ src/component/Select.tsx
@@ -7,15 +7,15 @@ type SelectProps<T extends string> = {
 
 export default function Select<T extends string>({ option, value, onChange, label }: SelectProps<T>) {
     return (
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2 bg-background rounded">
             {label && <label className="text-sm font-medium text-gray-700">{label}</label>}
             <select
                 value={value}
                 onChange={(e) => onChange(e.target.value as T)}
-                className="border border-r-2 bg-background px-2 py-1"
+                className="rounded bg-background px-2 py-1"
             >
                 {option.map((opt) => (
-                    <option key={opt} value={opt}>
+                    <option key={opt} value={opt} className="border border-r-2">
                         {opt}
                     </option>
                 ))}
